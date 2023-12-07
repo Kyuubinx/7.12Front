46e9d4732fec4355d6e87653ee4683eb78536b0e
Lucas <kyuubinx@gmail.com>
2023-12-07 19:50:27 -0300

    aaaa

---
+++ lista/script.js
@@ -0,0 +1,19 @@
+const myHeaders = {
+    "Content-Type" : "application/json"
+}
+async function lista() { 
+    const res = await fetch("http://localhost:3001/produtos")
+    const prod = await res.json()
+    const ul = document.querySelector("ul")
+    ul.innerHTML = ""
+    prod.forEach((produtos) => {
+        ul.insertAdjacentHTML("beforeend",
+        `<li> id="lista-li">
+            <p>Nome: ${produtos.nome}</p>
+            <p>Custo: R$ ${parseFloat(produtos.custo).toFixed(2)}</p>
+            <p>Venda: R$ ${parseFloat(produtos.venda).toFixed(2)}</p>
+        </li>`)        
+    });
+    
+}
+lista()
